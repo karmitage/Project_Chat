47012b3a4f0ddf4e0201d7163ec90daa8265f607
unknown <archivecoderzero@gmail.com>
2019-07-13 10:31:33 -0700

    updated gitignore to not include raphael branch

--- login_test_raphael-branch/config/keys.js
+++
@@ -1,8 +0,0 @@
-dbPassword = 'mongodb+srv://new-user_31:'+ encodeURIComponent('samplelogin') + '@cluster0-cod3w.mongodb.net/test?retryWrites=true&w=majority';
-
-
-
-
-module.exports = {
-    mongoURI: dbPassword
-};
