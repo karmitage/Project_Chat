c7d4019351076db8818a7aebe7b428736bbb106a
unknown <archivecoderzero@gmail.com>
2019-07-12 23:02:20 -0700

    added test login page using mongoDB for authentication

---
+++ login_test_raphael-branch/config/keys.js
@@ -0,0 +1,8 @@
+dbPassword = 'mongodb+srv://new-user_31:'+ encodeURIComponent('samplelogin') + '@cluster0-cod3w.mongodb.net/test?retryWrites=true&w=majority';
+
+
+
+
+module.exports = {
+    mongoURI: dbPassword
+};
